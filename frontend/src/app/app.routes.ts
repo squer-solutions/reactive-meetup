9caf98089d1ee47c8f6c9fbca9d287075c9c9832
leonqb <leon.quattlebaum@squer.io>
2024-11-19 17:55:44 +0100

    add samples for frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ReactiveUpdate } from './samples/reactive-update';
+import { LogFeedComponent } from './log-feed/log-feed.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LogFeedComponent, pathMatch: 'full'},
+  {path: 'reactive-update', component: ReactiveUpdate, pathMatch: 'full'},
+];
