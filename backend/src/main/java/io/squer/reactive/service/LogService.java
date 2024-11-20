f683145df4581b89fac62b20c23771e15cb6e627
leonqb <leon.quattlebaum@squer.io>
2024-11-20 15:31:20 +0100

    remove classes for demo

--- backend/src/main/java/io/squer/reactive/service/LogService.java
+++ backend/src/main/java/io/squer/reactive/service/LogService.java
@@ -5,28 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class LogService {
 
-    private final List<LogProvider> logProviders;
-
-    public Flux<LogEntry> getLogs(String filter) {
-        return Flux.merge(
-            logProviders.stream()
-                .map(LogProvider::getLogs)
-                .toList()
-        ).filter(logEntry -> logEntry.message().contains(filter));
-    }
-
     public Flux<LogEntry> getLogs() {
-        return Flux.merge(
-            logProviders.stream()
-                .map(LogProvider::getLogs)
-                .toList()
-        );
+        throw new UnsupportedOperationException("Not implemented");
     }
 
 }
