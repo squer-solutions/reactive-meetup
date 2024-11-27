fc54b701775968d673b1aae94389fc46de696f87
leonqb <leon.quattlebaum@squer.io>
2024-11-27 17:08:33 +0100

    add log providers

--- backend/src/main/java/io/squer/reactive/service/LogService.java
+++ backend/src/main/java/io/squer/reactive/service/LogService.java
@@ -3,12 +3,15 @@ package io.squer.reactive.service;
 import io.squer.reactive.model.LogEntry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class LogService {
+
+    private final List<LogProvider> logProviders;
 
     public Flux<LogEntry> getLogs(String filterText) {
         throw new UnsupportedOperationException("Not implemented");
