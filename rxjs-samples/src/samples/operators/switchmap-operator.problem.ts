4261795f3d43bbffaaabf870940820444f80b163
Niyati <niyati.rana@squer.io>
2024-11-27 14:53:50 +0100

    updated switch map problem

--- rxjs-samples/src/samples/operators/switchmap-operator.problem.ts
+++ rxjs-samples/src/samples/operators/switchmap-operator.problem.ts
@@ -2,16 +2,16 @@ import {interval, map, Subscription, take} from "rxjs";
 import { logger } from "../../logger/pretty-logger";
 
 export function testSwitchMapProblem() {
-    const userInput = ["S", "SQ", "SQU", "SQUE", "SQUER"];
-    const userInputStream$ = interval(3000).pipe(
-        take(userInput.length),
-        map(idx => userInput[idx])
+    const names = ["alice", "bob", "gerald", "hans", "michael"];
+    const namesStream$ = interval(3000).pipe(
+        take(names.length),
+        map(idx => names[idx])
     );
 
     let currentInnerSubscription: Subscription | null = null;
 
-    userInputStream$.subscribe(userInput => {
-        logger.log(`starting inner stream for user input ${userInput}`, 'yellow');
+    namesStream$.subscribe(name => {
+        logger.log(`starting inner stream for ${name}`, 'yellow');
 
         // Cancel the previous inner subscription if it exists
         if (currentInnerSubscription) {
@@ -21,6 +21,7 @@ export function testSwitchMapProblem() {
         // Start a new inner stream
         currentInnerSubscription = interval(1000).pipe(
             take(5),
+            map(num => name + num)
         ).subscribe(res => logger.log(res, 'blue'));
     });
 }
