2806d2ceed5cdd5185700a13623e7b2747cb64a7
leonqb <leon.quattlebaum@squer.io>
2024-11-27 10:43:22 +0100

    add mergemap problem

--- rxjs-samples/src/index.ts
+++ rxjs-samples/src/index.ts
@@ -1,24 +1,3 @@
-import { basicReactive } from "./samples/basic-reactive";
-import { testMulticast } from "./samples/multicast";
-import { testConcatMap } from "./samples/operators/concatmap-operator";
-import { testDebounce } from "./samples/operators/debounce-operator";
-import { testFilterOperator } from "./samples/operators/filter-operator";
-import { testMapAndFilter } from "./samples/operators/map-filter";
-import { testMapOperator } from "./samples/operators/map-operator";
-import { testMergeMap } from "./samples/operators/mergemap-operator";
-import { testSwitchMap } from "./samples/operators/switchmap-operator";
-import { testBehaviorSubject } from "./samples/subjects/behavior-subject";
-import { testReplaySubject } from "./samples/subjects/replay-subject";
-import { testSubject } from "./samples/subjects/subject";
-import { testUnicast } from "./samples/unicast";
-import {testConcatProblem} from "./samples/operators/concat-operator.problem";
-import {testConcat} from "./samples/operators/concat-operator";
-import {testCombineLatestProblem} from "./samples/operators/combinelatest-operator.problem";
-import {testCombineLatest} from "./samples/operators/combinelatest-operator";
-import {testZip} from "./samples/operators/zip-operator";
-import {testZipProblem} from "./samples/operators/zip-operator.problem";
-import {testMapAndFilterProblem} from "./samples/operators/map-filter.problem";
-
 // basicReactive();
 
 // testMapAndFilterProblem();
@@ -27,8 +6,9 @@ import {testMapAndFilterProblem} from "./samples/operators/map-filter.problem";
 // testUnicast();
 // testMulticast();
 // testDebounce();
-// testSwitchMap();
+// testMergeMapProblem();
 // testMergeMap();
+// testSwitchMap();
 // testConcatMap();
 
 // testSubject();
---
+++ rxjs-samples/src/samples/operators/mergemap-operator.problem.ts
@@ -0,0 +1,22 @@
+import { interval, map, Subject, take } from "rxjs";
+import { logger } from "../../logger/pretty-logger";
+
+export function testMergeMapProblem() {
+    const names = ["alice", "bob", "gerald", "hans", "michael"];
+    const namesStream$ = interval(3000).pipe(
+        take(5),
+        map(idx => names[idx])
+    );
+
+    const a = new Subject();
+
+    namesStream$.subscribe(name => {
+        logger.log(`starting inner stream for ${name}`, 'yellow');
+        interval(1000).pipe(
+            take(names.length),
+            map(val => name + val)
+        ).subscribe(res => a.next(res));
+    })
+
+    a.subscribe(res => logger.log(res, 'blue'));
+}
