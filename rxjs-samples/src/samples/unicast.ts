cdb70080d0e0e10b78268de21294a40bcb38092b
Niyati <niyati.rana@squer.io>
2025-06-16 10:52:04 +0200

    modified docker compose to use 3.9.x versions, added logs

--- rxjs-samples/src/samples/unicast.ts
+++ rxjs-samples/src/samples/unicast.ts
@@ -4,10 +4,12 @@ import { logger } from '../logger/pretty-logger';
 export function testUnicast() {
   const unicast = interval(1000).pipe(take(10));
 
+  // This subscriber is going to log out YELLOW messages
   unicast.subscribe(value => logger.log(value * 10, 'yellow'));
 
   setTimeout(() => {
     logger.log('Subscribing again...', 'blue');
+    // This subscriber is going to log out BLUE messages
     unicast.subscribe(value => logger.log(value * 100, 'blue'));
   }, 4000);
 }
